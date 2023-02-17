04046d938a76632e78f39aae774f1c0551d46c7b
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-17 10:55:37 +0600

    new starting files

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { text } = require("body-parser");
-const _ = require('lodash');
-
-let BlogPost = [];
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,50 +12,58 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-app.locals._ = _;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res){
-  res.render("home.ejs", {homeStartingContent: homeStartingContent, BlogPost:BlogPost});
-})
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
 
 app.get("/about", function(req, res){
-  res.render("about.ejs", {aboutContent: aboutContent});
-})
+  res.render("about", {aboutContent: aboutContent});
+});
 
 app.get("/contact", function(req, res){
-  res.render("contact.ejs", {contactContent: contactContent});
-})
+  res.render("contact", {contactContent: contactContent});
+});
 
 app.get("/compose", function(req, res){
-  res.render("compose.ejs");
+  res.render("compose");
+});
 
-  app.post("/", function(req, res){
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
 
-    BlogPost.push({      
-      postTitle : req.body.title,
-      postBody : req.body.post
-    })
+  posts.push(post);
 
-    res.redirect("/");
-  })
-})
+  res.redirect("/");
 
-app.get("/post/:text", (req, res) => {
+});
 
-  let sTitle = _.lowerCase(req.params.text)
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
 
-  BlogPost.forEach(element => {
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
 
-    let rTitle = _.lowerCase(element.postTitle);
-
-    if (sTitle===rTitle){
-      res.render("post.ejs", {Title: element.postTitle, Content: element.postBody});
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
-  })
-})
+  });
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
